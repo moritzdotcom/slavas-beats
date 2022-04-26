ea4837525ddd8e4dfe9a0c3fc703172ab7d71de8
Moritz Löchner <momo.loechner@gmail.com>
2022-04-27 01:44:18 +0200

    Images

--- pages/_document.js
+++ pages/_document.js
@@ -22,6 +22,31 @@ class SlavasBeatsDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link rel="icon" href="/favicon.ico" />
           <link rel="shortcut icon" href="/favicon.ico" />
+
+          <meta name="twitter:card" content="Mint Your SlavasBeatsNFT" />
+          <meta name="twitter:url" content="https://slavasbeats.com" />
+          <meta name="twitter:title" content="SlavasBeatsNFT" />
+          <meta
+            name="twitter:description"
+            content="Minting Page for the Slavas Beats NFT"
+          />
+          <meta
+            name="twitter:image"
+            content="https://slavasbeats.com/images/homescreen192.png"
+          />
+          <meta name="twitter:creator" content="@moritzloechner" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="SlavaBeatsNFT" />
+          <meta
+            property="og:description"
+            content="Minting Page for the Slavas Beats NFT"
+          />
+          <meta property="og:site_name" content="SlavaBeatsNFT" />
+          <meta property="og:url" content="https://slavasbeats.com" />
+          <meta
+            property="og:image"
+            content="https://slavasbeats.com/images/homescreen512.png"
+          />
         </Head>
         <body>
           <Main />
